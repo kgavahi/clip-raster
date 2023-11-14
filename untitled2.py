9d52315c0d6c87ce328d2966c1283c4bed433d89
kayhangavahi <kayhangavahi@gmail.com>
2023-11-14 13:52:41 -0600

    nldas download solved

--- untitled2.py
+++ untitled2.py
@@ -1,101 +1,19 @@
-import requests # get the requsts library from https://github.com/requests/requests
- 
- 
- 
-# overriding requests.Session.rebuild_auth to mantain headers when redirected
- 
-class SessionWithHeaderRedirection(requests.Session):
- 
-    AUTH_HOST = 'urs.earthdata.nasa.gov'
- 
-    def __init__(self, username, password):
- 
-        super().__init__()
- 
-        self.auth = (username, password)
- 
-  
- 
-   # Overrides from the library to keep headers when redirected to or from
- 
-   # the NASA auth host.
- 
-    def rebuild_auth(self, prepared_request, response):
- 
-        headers = prepared_request.headers
- 
-        url = prepared_request.url
- 
-  
- 
-        if 'Authorization' in headers:
- 
-            original_parsed = requests.utils.urlparse(response.request.url)
- 
-            redirect_parsed = requests.utils.urlparse(url)
- 
-  
- 
-            if (original_parsed.hostname != redirect_parsed.hostname) and redirect_parsed.hostname != self.AUTH_HOST and original_parsed.hostname != self.AUTH_HOST:
- 
-                del headers['Authorization']
- 
-  
- 
-        return
- 
-  
- 
-# create session with the user credentials that will be used to authenticate access to the data
- 
-username = "kgavahi"
- 
-password= "491Newyork"
- 
-session = SessionWithHeaderRedirection(username, password)
- 
-  
- 
-# the url of the file we wish to retrieve
- 
-url = "http://e4ftl01.cr.usgs.gov/MOLA/MYD17A3H.006/2009.01.01/MYD17A3H.A2009001.h12v05.006.2015198130546.hdf.xml"
- 
-  
- 
-# extract the filename from the url to be used when saving the file
- 
-filename = url[url.rfind('/')+1:]  
- 
-  
- 
-try:
- 
-    # submit the request using the session
- 
-    response = session.get(url, stream=True)
- 
-    print(response.status_code)
- 
-  
- 
-    # raise an exception in case of http errors
- 
-    response.raise_for_status()  
- 
-  
- 
-    # save the file
- 
-    with open(filename, 'wb') as fd:
- 
-        for chunk in response.iter_content(chunk_size=1024*1024):
- 
-            fd.write(chunk)
- 
-  
- 
-except requests.exceptions.HTTPError as e:
- 
-    # handle any errors here
- 
-    print(e)
+year = 2023
+numday = 1
+date = '20230101'
+h = 0
+
+url = f'https://hydro1.gesdisc.eosdis.nasa.gov/daac-bin/OTF/HTTP_services.cgi?\
+FILENAME=%2Fdata%2FNLDAS%2FNLDAS_FORA0125_H.002%2F{year}%2F{numday:03d}\
+%2FNLDAS_FORA0125_H.A{date}.{h:02d}00.002.grb&FORMAT=bmM0Lw&BBOX=25%2C\
+-125%2C53%2C-67&LABEL=NLDAS_FORA0125_H.A{date}.{h:02d}00.002.grb.SUB.nc4\
+&SHORTNAME=NLDAS_FORA0125_H&SERVICE=L34RS_LDAS&VERSION=1.02&DATASET_VERSION=002'
+
+print(f'wget --load-cookies .urs_cookies --save-cookies .urs_cookies\
+ --keep-session-cookies --user=kgavahi --ask-password --content-disposition {url}')
+
+
+import os
+
+os.system(f'wget --load-cookies .urs_cookies --save-cookies .urs_cookies\
+ --keep-session-cookies --user=kgavahi --password=491Newyork --content-disposition "{url}"')
