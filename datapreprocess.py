05750b998f2211840f7fa215c44000f7b0729273
kayhangavahi <kayhangavahi@gmail.com>
2023-12-21 10:05:52 -0600

    Update datapreprocess.py

--- datapreprocess.py
+++ datapreprocess.py
@@ -477,7 +477,10 @@ dp.dl_persiann(path='chirps',
             )
 
 
-
+dp = DataPreprocess(user='kgavahi', password='491Newyork')
+dp.dl_persiann(path='chirps',
+            start_date='19820101', end_date='19900101',
+            )
 
   
 
