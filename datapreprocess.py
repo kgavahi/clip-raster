f2149f2a666332ec9995cecdf6de5aee1039e496
kayhangavahi <kayhangavahi@gmail.com>
2024-02-03 15:09:19 -0600

    Update datapreprocess.py

--- datapreprocess.py
+++ datapreprocess.py
@@ -492,7 +492,7 @@ import matplotlib.pyplot as plt
 da1 = xr.open_dataset('201001140000.LSMOUT_DOMAIN1')
 swdown = np.array(da1.SMC)
 swdown = np.where(swdown<0, np.nan, swdown)
-plt.pcolormesh(swdown[0, :, 0, :])
+plt.pcolormesh(swdown[0, :, 1, :])
 
 fig = plt.gcf()
 
